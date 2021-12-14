76987829984d7b268327c78b9081ad21331dd08a
SHAHKUL <94035362+SHAHKUL@users.noreply.github.com>
2021-12-14 21:16:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("submit").onclick= function(){
+     var fname=document.getElementById("Firstname").value;
+     console.log(fname);
+
+     var lname=document.getElementById("Lastname").value;
+     console.log(lname);
+
+     var mname=document.getElementById("Middlename").value;
+     console.log(mname);
+
+     var email=document.getElementById("email").value;
+     console.log(email);
+    }
